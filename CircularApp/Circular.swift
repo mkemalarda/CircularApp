1a70c074d290a3a992c09aed012086a5a7733c2b
Mustafa Kemal ARDA <mkemalarda@gmail.com>
2023-12-18 00:05:57 +0300

    color data added

--- CircularApp/Circular.swift
+++ CircularApp/Circular.swift
@@ -8,13 +8,20 @@
 import SwiftUI
 
 struct Circular: View {
+    
+    let lineWidth : CGFloat
+    let backgroundColor : Color
+    let foregroundColor : Color
+    let percentage : Double
+    
     var body: some View {
         CircularShape()
-        
+            .stroke(style: StrokeStyle(lineWidth: lineWidth))       // çerçevenin kalınlığını belirler
+            .fill(backgroundColor)                                                 // şeklin güzel gözükmesini sağlar
         
     }
 }
 
 #Preview {
-    Circular()
+    Circular(lineWidth: 10, backgroundColor: .red, foregroundColor: .black, percentage: 50)
 }
