0953744a7df755fabbc3649a34de9092953509e3
Mustafa Kemal ARDA <mkemalarda@gmail.com>
2023-12-17 23:58:35 +0300

    circular files added

---
+++ CircularApp/Circular.swift
@@ -0,0 +1,20 @@
+//
+//  Circular.swift
+//  CircularApp
+//
+//  Created by Mustafa Kemal ARDA on 17.12.2023.
+//
+
+import SwiftUI
+
+struct Circular: View {
+    var body: some View {
+        CircularShape()
+        
+        
+    }
+}
+
+#Preview {
+    Circular()
+}
