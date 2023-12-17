d41f96550e2de4a0ca2de397721889f6ae34b8ce
Mustafa Kemal ARDA <mkemalarda@gmail.com>
2023-12-17 23:34:04 +0300

    Initial Commit

---
+++ CircularApp/CircularAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularAppApp.swift
+//  CircularApp
+//
+//  Created by Mustafa Kemal ARDA on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CircularAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
